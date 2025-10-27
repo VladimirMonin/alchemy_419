4868495feac18e2922c16bbc980936010197a5b6
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-10-27 20:50:03 +0500

    base_class: - Сделал базовый класс - Сделал пакет с моделями - Подготовил модуль для описания моделей

---
+++ models/base.py
@@ -0,0 +1,12 @@
+# models/base.py
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """
+    Базовая модель для всех моделей SQLAlchemy.
+    """
+
+    pass
+
+
---
+++ models/models.py
@@ -0,0 +1,4 @@
+# models/models.py
+from models.base import Base
+from sqlalchemy import String, Float, Integer
+from sqlalchemy.orm import Mapped, mapped_column
